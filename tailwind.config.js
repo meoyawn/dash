1830d46a933d103951b4f32b16e0b8bf75f1798d
Adel Nizamutdinov <stiggpwnz@gmail.com>
2021-04-20 13:46:45 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// See https://tailwindcss.com/docs/configuration for details
+module.exports = {
+  mode: 'jit',
+  darkMode: 'media',
+  plugins: [
+
+  ],
+  purge: {
+    content: [
+      './src/**/*.{jsx,tsx}'
+    ]
+  },
+  theme: {
+
+  },
+  variants: {},
+}
